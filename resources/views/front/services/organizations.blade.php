d17161b7d98bf79e44613da7703a52556cfb74db
Michael Joyner <joyner.michael@gmail.com>
2018-06-15 11:30:16 +0800

    text colour in nav and mobile cards in service orgs

--- resources/views/front/services/organizations.blade.php
+++ resources/views/front/services/organizations.blade.php
@@ -1,7 +1,7 @@
 <section id="organizations">
     <div class="max-w-lg mx-auto my-28">
         <h1 class="text-pink h1 text-center mb-28">{{ trans('services.organizations.heading') }}</h1>
-        <div class="flex flex-col md:flex-row bg-off-white rounded md:flex-row-reverse shadow-none md:shadow my-12">
+        <div class="flex flex-col md:flex-row bg-off-white rounded md:flex-row-reverse mx-4 md:mx-0 shadow my-12">
             <div class="w-full md:w-1/2 flex justify-center items-center">
                 @include('front.svgicons.organization_1', ['svgClass' => 'h-12 md:h-32'])
             </div>
@@ -11,7 +11,7 @@
             </div>
 
         </div>
-        <div class="flex flex-col md:flex-row bg-off-white rounded shadow-none md:shadow my-12">
+        <div class="flex flex-col md:flex-row bg-off-white rounded mx-4 md:mx-0 shadow my-12">
             <div class="w-full md:w-1/2 flex justify-center items-center">
                 @include('front.svgicons.organization_2', ['svgClass' => 'h-12 md:h-32'])
             </div>
@@ -20,7 +20,7 @@
                 <p class="body mt-4 text-center max-w-xs mx-auto">{{ trans('services.organizations.networking.content') }}</p>
             </div>
         </div>
-        <div class="flex flex-col md:flex-row bg-off-white rounded md:flex-row-reverse shadow-none md:shadow my-12">
+        <div class="flex flex-col md:flex-row bg-off-white rounded md:flex-row-reverse mx-4 md:mx-0 shadow my-12">
             <div class="w-full md:w-1/2 flex justify-center items-center">
                 @include('front.svgicons.organization_3', ['svgClass' => 'h-12 md:h-32'])
             </div>
@@ -30,7 +30,7 @@
             </div>
 
         </div>
-        <div class="flex flex-col md:flex-row bg-off-white rounded shadow-none md:shadow my-12">
+        <div class="flex flex-col md:flex-row bg-off-white rounded mx-4 md:mx-0 shadow my-12">
             <div class="w-full md:w-1/2 flex justify-center items-center">
                 @include('front.svgicons.organization_4', ['svgClass' => 'h-12 md:h-32'])
             </div>
