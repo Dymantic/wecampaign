7e290df6dfd96739fe795f4a5f79c289261aa5a3
Michael Joyner <joyner.michael@gmail.com>
2018-07-05 12:27:23 +0800

    small colour change to top button

--- resources/views/front/partials/page-ender.blade.php
+++ resources/views/front/partials/page-ender.blade.php
@@ -1,5 +1,5 @@
 <section class="py-28 text-center">
-    <a href="#" data-jump-target="body" data-jump-offset="96" class="inline-flex flex-col text-blue-light hover:text-blue-dark mt-12 no-underline">
+    <a href="#" data-jump-target="body" data-jump-offset="96" class="inline-flex flex-col text-blue-dark hover:text-white mt-12 no-underline">
         <span class="">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="50px" class="fill-current -mb-4"><path class="heroicon-ui" d="M8.7 14.7a1 1 0 0 1-1.4-1.4l4-4a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1-1.4 1.4L12 11.42l-3.3 3.3z"/></svg>
         </span>
