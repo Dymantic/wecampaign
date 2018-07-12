b215b6e7f7ba34a62686adc79efeb7b249c40fb2
Michael Joyner <joyner.michael@gmail.com>
2018-07-12 13:45:49 +0800

    add padding to app preview section

--- resources/views/front/home/app-preview.blade.php
+++ resources/views/front/home/app-preview.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-lg mx-auto py-28">
         <h1 class="h1 px-4 text-center text-blue-dark">@lang('homepage.app_preview.heading')</h1>
     </div>
-    <picture class="max-w-md w-full mx-auto block">
+    <picture class="max-w-md w-full mx-auto block pb-28">
         <source srcset="/images/app_preview_horizontal.png" media="(min-width: 573px)">
         <source srcset="/images/app_preview_vertical.png" media="(max-width: 572px)">
         <img src="/images/app_preview_horizontal.png"
